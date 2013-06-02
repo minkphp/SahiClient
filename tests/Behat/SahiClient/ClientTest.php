6eefa2700c4ef9f7870f1c79bf14fdeed9d09b2d
Alexander Obuhovich <aik.bold@gmail.com>
2013-06-02 22:08:02 +0300

    Return result of $conditionResult after `wait` method call. Tests added.
    Needed for https://github.com/Behat/Mink/issues/301

--- tests/Behat/SahiClient/ClientTest.php
+++ tests/Behat/SahiClient/ClientTest.php
@@ -7,6 +7,12 @@ require_once 'AbstractConnectionTest.php';
 
 class ClientTest extends AbstractConnectionTest
 {
+
+    /**
+     * Client with a mocked connection.
+     *
+     * @var Client
+     */
     private $api;
 
     public function setUp()
@@ -43,6 +49,44 @@ class ClientTest extends AbstractConnectionTest
             array($this->api, 'setSpeed'),
             array(12)
         );
+    }
+
+    /**
+     * Tests, that wait method returns $conditionResult.
+     *
+     * @return void
+     * @access public
+     */
+    public function testWaitReturnTrue()
+    {
+        $connection = $this->api->getConnection();
+
+        $connection
+            ->expects($this->any())
+            ->method('evaluateJavascript')
+            ->with('found-element')
+            ->will($this->returnValue(true));
+
+        $this->assertTrue($this->api->wait(500, 'found-element'));
+    }
+
+    /**
+     * Tests, that wait method returns $conditionResult.
+     *
+     * @return void
+     * @access public
+     */
+    public function testWaitReturnFalse()
+    {
+        $connection = $this->api->getConnection();
+
+        $connection
+            ->expects($this->any())
+            ->method('evaluateJavascript')
+            ->with('not-found-element')
+            ->will($this->returnValue(false));
+
+        $this->assertFalse($this->api->wait(500, 'not-found-element'));
     }
 
     public function testFindByClassName()
